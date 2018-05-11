d28750a3a25e16c7a4d3ba9618009a3050d93ab5
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-05-11 11:29:49 -0500

    process.env.NODE_ENV="test"

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Common code for OAuth-based login services in clinical environments.",
-  version: "1.1.26",
+  version: "1.1.28",
   name: 'clinical:accounts-oauth',
   git: 'https://github.com/clinical-meteor/accounts-oauth'
 });
@@ -14,7 +14,7 @@ Package.onUse(function (api) {
 
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
-  api.use('clinical:oauth@1.2.11');
+  api.use('clinical:oauth@1.2.13');
 
   api.addFiles('oauth_common.js');
   api.addFiles('oauth_client.js', 'client');
