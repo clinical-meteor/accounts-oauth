44b9f310d9bae1b5352e59fb0661995c2ce3e819
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-03 17:49:30 -0500

    Accounts.oauth exists

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Common code for OAuth-based login services in clinical environments.",
-  version: "1.1.46",
+  version: "1.1.47",
   name: 'clinical:accounts-oauth',
   git: 'https://github.com/clinical-meteor/accounts-oauth'
 });
