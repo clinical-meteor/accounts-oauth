3405310d8970f4ae05850f8e798ecf7f7e02346b
Abigail Watson <awatson1978@gmail.com>
2018-10-08 21:25:07 -0400

    post-baltimore

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Common code for OAuth-based login services in clinical environments.",
-  version: "1.1.51",
+  version: "1.1.52",
   name: 'clinical:accounts-oauth',
   git: 'https://github.com/clinical-meteor/accounts-oauth'
 });
